ee7d2375ea190543e5927f52c773db15f91f84ea
Karthikeyan.A <144608853+karthi1048@users.noreply.github.com>
2024-12-25 16:11:27 +0530

    Update Day 2.sql with 2 solutions

--- basic SQL/Day 2.sql
+++ basic SQL/Day 2.sql
@@ -1,1 +1,13 @@
+/* Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru', there are 2 different city names: 'New York' and 'Bengalaru'. 
+The query returns 1.
+*/
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) FROM STATION;
 
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY LIKE 'A%' 
+OR CITY LIKE 'E%'
+OR CITY LIKE 'I%'
+OR CITY LIKE 'O%'
+OR CITY LIKE 'U%';
