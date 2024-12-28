ec3ca0fb0395c1bbbb7dc078d09ffcbb784a433a
Karthikeyan.A <144608853+karthi1048@users.noreply.github.com>
2024-12-28 08:05:56 +0530

    Update Day 5.sql with 2 solutions

--- basic SQL/Day 5.sql
+++ basic SQL/Day 5.sql
@@ -1,1 +1,7 @@
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(population) FROM CITY WHERE COUNTRYCODE = 'JPN';
 
+-- Query the difference between the maximum and minimum populations in CITY.
+SELECT MAX(population) - MIN(population) FROM CITY;
+
+-- 
