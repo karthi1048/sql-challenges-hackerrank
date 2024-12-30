65129ef850173b55878de36fe67bec3183b768ac
Karthikeyan.A <144608853+karthi1048@users.noreply.github.com>
2024-12-30 14:19:20 +0530

    Update Day 7.sql with 1 solution

--- basic SQL/Day 7.sql
+++ basic SQL/Day 7.sql
@@ -1,1 +1,5 @@
-
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT COUNTRY.continent, FLOOR(AVG(CITY.population)) FROM CITY
+JOIN COUNTRY ON CITY.countrycode = COUNTRY.code
+GROUP BY COUNTRY.continent;
