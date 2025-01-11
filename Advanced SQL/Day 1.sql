cfee4cd440f8929616fc7bc2a07c2eedc5dc42ac
Karthikeyan.A <144608853+karthi1048@users.noreply.github.com>
2025-01-11 19:01:16 +0530

    Update Day 1.sql with 2 solutions

--- Advanced SQL/Day 1.sql
+++ Advanced SQL/Day 1.sql
@@ -1,1 +1,39 @@
+/*
+P(R) represents a pattern drawn by Julia in R rows. 
+The following pattern represents P(5):
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+Write a query to print the pattern P(20).
+*/
+WITH RECURSIVE Pattern(row_num, stars) 
+AS (
+    SELECT 20, REPEAT('* ', 20)
+    UNION ALL
+    SELECT row_num - 1, REPEAT('* ', row_num - 1)
+    FROM Pattern 
+    WHERE row_num > 1
+)
+SELECT stars FROM Pattern;
 
+/*
+P(R) represents a pattern drawn by Julia in R rows. 
+The following pattern represents P(5):
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+Write a query to print the pattern P(20).
+*/
+WITH RECURSIVE Pattern(row_num, stars) 
+AS (
+    SELECT 1, CAST(REPEAT('* ', 1) AS CHAR(1000))
+    UNION ALL
+    SELECT row_num + 1, CAST(REPEAT('* ', row_num + 1) AS CHAR(1000))
+    FROM Pattern
+    WHERE row_num < 20
+)
+SELECT stars FROM Pattern;
